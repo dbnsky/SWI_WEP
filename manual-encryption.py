5e5a80d00fc9ae6d0160adb8f96ac6e5f556b5e0
Théo Gallandat <theo.gallandat@heig-vd.ch>
2018-04-05 17:02:44 +0200

    part 2 commented

--- manual-encryption.py
+++ manual-encryption.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" Manually decrypt a wep message given the WEP key"""
+""" Manually encrypt a wep message given the WEP key"""
 
-__author__      = "Abraham Rubinstein"
+__author__      = "Nguefack Zacharie, Gallandat Théo, Emmanuel Schmid"
 __copyright__   = "Copyright 2017, HEIG-VD"
 __license__ 	= "GPL"
 __version__ 	= "1.0"
-__email__ 		= "abraham.rubinstein@heig-vd.ch"
-__status__ 		= "Prototype"
 
 from scapy.all import *
 import binascii
@@ -17,71 +15,49 @@ import rc4
 #Cle wep AA:AA:AA:AA:AA
 key='\xaa\xaa\xaa\xaa\xaa'
 
-#lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
+print "Key: " + key.encode("hex")
+# Récupération de la trame "template"
 arp = rdpcap('arp.cap')[0]  
 
 # rc4 seed est composé de IV+clé
-seed = arp.iv+key 
+seed = arp.iv + key
 
-print 'initalil arp.icv '+str(arp.icv)
-# recuperation de icv dans le message (arp.icv) (en chiffre) -- je passe au format "text". Il y a d'autres manières de faire ceci...
-icv_encrypted='{:x}'.format(arp.icv).decode("hex")
+print "Seed: " + seed.encode("hex")
 
-# text chiffré y-compris l'icv
-message_encrypted=arp.wepdata+icv_encrypted 
+# Le messagee en clair
+plaintxt="ThisMessageWasEncryptedAndNowItIsNot!"
 
-# déchiffrement avec rc4
-cleartext=rc4.rc4crypt(message_encrypted,seed)  
+print "Text Msg: " + plaintxt
 
-# le ICV est les derniers 4 octets - je le passe en format Long big endian
-icv_enclair=cleartext[-4:]
+#Création de l'icv
+icv=(binascii.crc32(plaintxt) & 0xFFFFFFFF)
 
-print 'icv en clair' + icv_enclair.encode("hex")
+print "Base ICV: " + str(icv)
 
-(icv_numerique,)=struct.unpack('!L', icv_enclair)
+#Converstion de l'icv en littleEndian
+icv_litleEndian=struct.pack('<L', icv)
 
-# le message sans le ICV
-text_enclair=cleartext[:-4] 
-	
-print 'Text: ' + text_enclair.encode("hex")
-print 'icv:  ' + icv_enclair.encode("hex")
-print 'icv(num): ' + str(icv_numerique)
-print 'icv chiffre: ' + icv_encrypted.encode("hex")
+print "ICV littleEndian: " + icv_litleEndian.encode("hex")
 
-
-## TASK
-
-# rc4 seed est composé de IV+clé
-seed = arp.iv+key
-
-# Le message sans le ICV
-plaintxt="sdsdsdkjdksjdksdjsdkjskdjsdsdsdkjdksjdksdjsdkjskdjsdsdsdkjdksjdksdjsdkjskdjsdsdsdkjdksjdksdjsdkjskdjsdsdsdkjdksjdksdjsdkjskdjsdsdsdkjdksjdksdjsdkjskdj"
-
-icv_plaintxt=(binascii.crc32(plaintxt) & 0xFFFFFFFF)
-
-icv_litleEndian=struct.pack('<L', icv_plaintxt)
-
-# Le flux à chiffrer composé de l'icv du bloc
-fluxClearTxt = plaintxt + icv_litleEndian
+# Le flux à chiffrer composé de l'icv du bloc et du message en clair
+streamClearTxt = plaintxt + icv_litleEndian
 
 # Chiffrement à l'aide de rc4
+encryptedTxt=rc4.rc4crypt(streamClearTxt,seed)
 
-cyphetxt=rc4.rc4crypt(fluxClearTxt,seed)
+print "Encrypted Text Msg: " + encryptedTxt.encode("hex")
 
 #MAJ contenu msg chiffré (msg + icv)
-arp.wepdata = cyphetxt[:-4]
+arp.wepdata = encryptedTxt[:-4]
+
 #MAJ contenu ICV (icv chiffré)
-icv_chiff = cyphetxt[-4:]
+icv_chiff = encryptedTxt[-4:]
 
-(icv_test,) = struct.unpack('!L', icv_chiff)
+(arp.icv,) = struct.unpack('!L', icv_chiff)
 
-arp.icv = icv_test
+wrpcap('exo2.pcap',arp, append=False)
 
-print arp.icv
-
-
-
-wrpcap('exo1.pcap',arp, append=False)
+print "Output created : exo2.pcap"
 
 
 
